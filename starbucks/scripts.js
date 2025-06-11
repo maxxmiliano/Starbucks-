6399b93608ae1365bfff7cd97bb018e3e0beab15
maxmiliano <maxmilianossouza@hotmail.com>
2025-06-11 14:43:13 -0300

    dando andamento no prejeto

---
+++ starbucks/scripts.js
@@ -0,0 +1,11 @@
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".Starbucks")
+
+function trocaCor(cor){
+    circulo.style.background = cor
+}
+
+function trocaImagem(img){
+    imagem.src = img
+}
